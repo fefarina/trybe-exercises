c28b69999240b6a5a00344026ce7637121999689
fefarina <wfelipefarina@gmail.com>
2020-07-13 15:43:01 -0300

    Resolving exercise 3

---
+++ 4.1/script3.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 15;
+let c = 3;
+
+if(a > b && a > c) {
+    console.log(`O maior numero é ${a}`);
+} else if (b > a && b > c) {
+    console.log(`O maior número pe ${b}`);
+} else {
+    console.log(`O maior número é ${c}`)
+}
