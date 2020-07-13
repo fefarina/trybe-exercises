dc0d57b1d9da268d33f247168084ca6e36619069
fefarina <wfelipefarina@gmail.com>
2020-07-13 15:45:42 -0300

    Resolving exercise 4

---
+++ 4.1/script4.js
@@ -0,0 +1,9 @@
+let a = 15;
+
+if(a > 0) {
+    console.log(`O número ${a} é positivo`);
+} else if (a < 0) {
+    console.log(`O número ${a} é negativo`)
+} else {
+    console.log(`O número ${a} é zero!`)
+}
