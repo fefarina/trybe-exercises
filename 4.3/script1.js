1a54d2590719add37c86947ddc0d9ce4899507a5
fefarina <wfelipefarina@gmail.com>
2020-07-15 14:43:02 -0300

    Resolving exercise 1

---
+++ 4.3/script1.js
@@ -0,0 +1,12 @@
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+
+for(let i = 0; i < n; i++){
+    inputLine = inputLine + symbol;
+}
+
+for (let i = 0; i < n; i++){
+    console.log(inputLine)
+}
