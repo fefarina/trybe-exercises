f1cb88b3933b21ead6801c5a2079f35533664ab1
wfelipefarina <wfelipefarina@gmail.com>
2020-07-21 09:19:17 -0300

    Update Exercises

---
+++ 4.4/function2.js
@@ -0,0 +1,13 @@
+function compareArray(list) {
+    let result = 0;
+    
+    for(let i = 0; i < list.length; i++) {
+        if(list[result] < list[i]) {
+            result = i
+        }
+    }
+    return result
+}
+
+
+console.log(compareArray([2, 3, 6, 17, 10, 1]))
