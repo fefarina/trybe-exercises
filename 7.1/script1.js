256a1ea5e3ead9ff8007bf2e4174ab859755cde1
wfelipefarina <wfelipefarina@gmail.com>
2020-09-09 14:03:22 -0300

    Bloco 11, React

--- 7.1/script1.js
+++ 7.1/script1.js
@@ -1,10 +1,16 @@
-let fatorial = number => {
-    let result = 1;
-    for(let i = 2; i <= number; i++) {
-        result *= i;
-    }
+// let fatorial = number => {
+//     let result = 1;
+//     for(let i = 2; i <= number; i++) {
+//         result *= i;
+//     }
 
-    return result
+//     return result
+// }
+
+// console.log(fatorial(4));
+
+const arr = [20, 5, 100, 1, 90, 200, 40, 29];
+
+for(let item of arr) {
+    setTimeout(() => console.log(item), item)
 }
-
-console.log(fatorial(4));
